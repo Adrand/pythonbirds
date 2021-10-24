ec8f595a56e26b5a6fec44f22fd36724f83b3bdc
Leal <adrianolealnascimento@gmail.com>
2021-10-24 17:07:43 -0300

    Estudo de composição finalizado

--- oo/carro.py
+++ oo/carro.py
@@ -68,30 +68,55 @@ O     L
     >>> direcao.girar_a_esquerda()
     >>> direcao.valor
     'Norte'
-    >>> carro = carro(direcao, motor)
-    >>> carro.calcular_velociade()
+    >>> carro = Carro(direcao, motor)
+    >>> carro.calcular_velocidade()
     0
-    >>> carro = carro(direcao, motor)
-    >>> carro.calcular_velociade()
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
     1
-    >>> carro = carro(direcao, motor)
-    >>> carro.calcular_velociade()
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
     2
-    >>> carro = carro(direcao, motor)
-    >>> carro.calcular_velociade()
+    >>> carro.frear()
+    >>> carro.calcular_velocidade()
     0
     >>> carro.calcular_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> carro.girar_a_direita()
     >>> carro.calcular_direcao()
-    >>> 'Leste'
+    'Leste'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    >>> 'Norte'
+    'Norte'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    >>> 'Oeste'
+    'Oeste'
 """
+
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 
 NORTE = 'Norte'
 SUL = 'Sul'
@@ -127,5 +152,3 @@ class Motor:
     def frear(self):
         self.velocidade -= 2
         self.velocidade = max(0, self.velocidade)
-
-
